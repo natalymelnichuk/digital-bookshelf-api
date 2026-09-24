35d558ed6f9f353e0f4438f5b3364007ba1ca59b
Natallia <ntshmlnchuk@gmail.com>
2026-09-24 15:31:04 -0400

    README completed

---
+++ README.md
@@ -0,0 +1,49 @@
+
+# Digital Bookshelf API
+
+A RESTful API for managing a digital book library built with Node.js, Express, and Mongoose (MongoDB Atlas). This project supports full CRUD functionality with data validation.
+
+
+## Tech Stack
+
+* **Runtime:** Node.js
+* **Framework:** Express.js
+* **Database:** MongoDB Atlas + Mongoose
+* **Logging & Utilities:** Morgan, dotenv
+
+
+## Prerequisites & Installation
+
+### Requirements
+
+Ensure you have the following installed on your machine:
+ * Node.js 
+ * npm 
+ * A MongoDB Atlas account (to obtain a database connection string)
+
+### Clone the Repository
+
+git clone (https://github.com/natalymelnichuk/digital-bookshelf-api)
+
+cd digital-bookshelf-api
+
+### Install Dependencies
+
+npm install
+
+### Configure Environment Variables (.env)
+
+Create a .env file in the root directory based on the template below:
+
+PORT=3000
+MONGO_URI=mongodb+srv://<username>:<password>@cluster0.mongodb.net/digital-bookshelf?retryWrites=true&w=majority
+
+## API Endpoints
+
+Base URL: http://localhost:3000/api/books
+
+POST - /api/books
+GET - /api/books
+GET - /api/books/:id
+PUT - /api/books/:id
+DELETE - /api/books/:id
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -6,13 +6,13 @@ const router = express.Router();
 // Create
 
 async function createBook (req, res) {
-  try {
-    const newBook = await Book.create(req.body);
-    res.status(201).json(newBook);
-  } catch(error) {
-    console.error(error);
-    res.status(400).json({ message: error.message });
-  }
+    try {
+        const newBook = await Book.create(req.body);
+        res.status(201).json(newBook);
+    } catch(error) {
+        console.error(error);
+        res.status(400).json({ message: error.message });
+    }
 }
 
 router.post("/", createBook);
@@ -20,13 +20,13 @@ router.post("/", createBook);
 // Read All
 
 async function fetchAllBooks(req, res) {
-  try {
-    const books = await Book.find({});
-    res.status(200).json(books);
-  } catch(error) {
-    console.error(error);
-    res.status(400).json({ message: error.message })
-  }
+    try {
+        const books = await Book.find({});
+        res.status(200).json(books);
+    } catch(error) {
+        console.error(error);
+        res.status(400).json({ message: error.message })
+    }
 }
 
 router.get("/", fetchAllBooks);
@@ -34,18 +34,18 @@ router.get("/", fetchAllBooks);
 // Read One 
 
 async function fetchOneBook (req, res) {
-  try {
-    const book = await Book.findById(req.params.id);
+    try {
+        const book = await Book.findById(req.params.id);
 
-    if (!book) {
-        return res.status(404).json({ message: "Book not found" });
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" });
+        }
+
+        res.json({ message: "Book fetched successfully!", book })
+    } catch(error) {
+        console.error(error);
+        res.status(400).json({ message: error.message })
     }
-
-    res.json({ message: "Book fetched successfully!", book })
-  } catch(error) {
-    console.error(error);
-    res.status(400).json({ message: error.message })
-  }
 }
 
 router.get("/:id", fetchOneBook);
@@ -53,18 +53,18 @@ router.get("/:id", fetchOneBook);
 // Update
 
 async function updateBook (req, res) {
-  try {
-    const book = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-    
-    if (!book) {
-      return res.status(404).json({ message: "Book not found" });
-    }
+    try {
+        const book = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        
+        if (!book) {
+        return res.status(404).json({ message: "Book not found" });
+        }
 
-    res.status(200).json(book);
-  } catch(error) {
-    console.error(error);
-    res.status(400).json({ message: error.message })
-  }
+        res.status(200).json(book);
+    } catch(error) {
+        console.error(error);
+        res.status(400).json({ message: error.message })
+    }
 }
 
 router.put("/:id", updateBook);
@@ -72,18 +72,18 @@ router.put("/:id", updateBook);
 // Delete 
 
 async function deleteBook (req, res) {
-  try {
-    const book = await Book.findByIdAndDelete(req.params.id);
+    try {
+        const book = await Book.findByIdAndDelete(req.params.id);
 
-    if (!book) {
-        return res.status(404).json({ message: "Book not found" });    
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" });    
+        }
+
+        res.json({ message: "Book deleted successfully!", book })
+    } catch(error) {
+        console.error(error);
+        res.status(400).json({ message: error.message })
     }
-
-    res.json({ message: "Book deleted successfully!", book })
-  } catch(error) {
-    console.error(error);
-    res.status(400).json({ message: error.message })
-  }
 }
 
 router.delete("/:id", deleteBook);
